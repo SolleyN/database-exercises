51760d1924ef0bd6645f22d589111ff56e707fba
Nickolas Solley <nicksolley@gmail.com>
2023-03-06 14:08:46 -0600

    where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+USE employees;
+
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'Q%';
