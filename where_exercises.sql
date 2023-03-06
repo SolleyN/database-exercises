ec4d19805c83dd06697f1f0a428253d01a972020
Nickolas Solley <nicksolley@gmail.com>
2023-03-06 14:29:52 -0600

    where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
-USE codeup_test_db;
+
 USE employees;
 
 SELECT * FROM employees
@@ -10,4 +10,26 @@ SELECT * FROM employees
 WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees
-WHERE last_name LIKE 'Q%';
+WHERE last_name LIKE 'Q
+%';
+
+SELECT * FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+
+SELECT * FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'M';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
